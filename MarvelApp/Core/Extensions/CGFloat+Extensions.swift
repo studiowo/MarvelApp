cab3533f60fe75641b5b5f2470f1598b713cafde
Renato Matos <contato@wostudio.com.br>
2019-08-02 01:02:36 -0300

    Remove unnecessary code

--- MarvelApp/Core/Extensions/CGFloat+Extensions.swift
+++ MarvelApp/Core/Extensions/CGFloat+Extensions.swift
@@ -10,22 +10,11 @@ import CoreGraphics
 import Foundation
 
 extension CGFloat {
-    
-    public var evenRounded: CGFloat {
+    var evenRounded: CGFloat {
         var newValue = self.rounded(.towardZero)
         if newValue.truncatingRemainder(dividingBy: 2) == 1 {
             newValue -= 1
         }
         return newValue
-    }
-    
-    public func interpolate(maxScroll: CGFloat, minScroll: CGFloat, max: CGFloat, min: CGFloat) -> CGFloat {
-        if self <= maxScroll && self >= minScroll {
-            return  max - ((self - minScroll) * ((max - min) / (maxScroll - minScroll)))
-        } else if self > maxScroll {
-            return min
-        } else {
-            return max
-        }
     }
 }
